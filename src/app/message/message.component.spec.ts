bcef4f3994285e9fb9d7fc96f4daa41845fa04c6
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-11-14 17:45:58 +0000

    Added tests to check for the api calls and a few other functionalities

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -23,5 +23,39 @@ describe('MessageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should toggle comment form', () => {
+    const message = { showCommentForm: false };
+    component.toggleCommentForm(message);
+    expect(message.showCommentForm).toBeTruthy();
+  });
+
+  it('should post a message', () => {
+    const httpSpy = spyOn(component['http'], 'post').and.callThrough();
+    component.postMessage();
+    expect(httpSpy).toHaveBeenCalledWith('http://localhost:3010/api/message', component.newMessage);
+    expect(component.newMessage).toEqual({ author: '', content: '' });
+  });
+
+  it('should add a comment', () => {
+    const httpSpy = spyOn(component['http'], 'post').and.callThrough();
+    const message = { _id: '1' };
+    component.addComment(message);
+    expect(httpSpy).toHaveBeenCalledWith('http://localhost:3010/api/message/1/comments', component.newComment);
+    expect(component.newComment).toEqual({ author: '', content: '' });
+  });
+
+  it('should update the view when messages change', () => {
+    component.messages = [{ content: 'Test Message' }];
+    fixture.detectChanges();
+    const messageElement = fixture.debugElement.query(By.css('.message p')).nativeElement;
+    expect(messageElement.textContent).toContain('Test Message');
+  });
+
+  it('should disable the submit button if the form is invalid', () => {
+    component.newMessage.author = '';
+    fixture.detectChanges();
+    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
+    expect(submitButton.disabled).toBeTruthy();
+  });
 
 });
